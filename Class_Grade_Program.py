8b907fc9fc520624d3d00a00e67f5c13ffa55912
markpadilla8 <78052733+markpadilla8@users.noreply.github.com>
2021-05-13 16:43:47 -0700

    finish exam

--- Class_Grade_Program.py
+++ Class_Grade_Program.py
@@ -23,3 +23,20 @@ print(number of grades)
 print(average grade)
 print(Percentage of the grade above average)
 """
+def main():
+    finaldict = create.dictionary("Final.txt")
+    displayinfoaboutfinalgrades(finaldict)
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+
+for i in range(len(grades)):
+    grades [i] = int(grades [i])
+average = sum(grades) / len(grades)
+num = 0
+for grade in grades:
+    if grade>average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2}%".format(100 * num / len(grades))
